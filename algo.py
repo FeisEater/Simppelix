17e88b7e58eb494f285efc13af5e549748f44eba
Pavel Smirnov <pasmpasm@cs.helsinki.fi>
2019-12-20 04:04:20 +0200

    Added setting first row basis columns to zero before main loop

--- algo.py
+++ algo.py
@@ -2,6 +2,19 @@ import numpy as np
 
 S = np.matrix([[-6.0,-3.0,-3.0,0.0,0.0,0.0],[1.0,3.0,2.0,1.0,0.0,0.0],[2.0,2.0,-1.0,0.0,1.0,0.0],[3.0,-1.0,3.0,0.0,0.0,1.0]])
 status = "calculating"
+SnoZero = S[1:,1:]
+colSize = SnoZero.shape[0]
+unit = np.zeros(colSize)
+basis = np.full(colSize, -1)
+for i in range(colSize):
+  unit[i] = 1.0
+  for j in range(SnoZero.shape[1]):
+    if np.all(np.transpose(SnoZero[:,j]) == unit):
+      basis[i] = j
+  unit[i] = 0
+for idx,col in enumerate(basis):
+  S[0,:] = S[0,:] - S[0,col+1]*S[idx+1,:]
+print(S)
 while True:
   negRow0 = S[0,1:] >= 0.0
   if np.all(negRow0):
@@ -15,10 +28,12 @@ while True:
     status = "unbounded"
     break
   pivotRowIdx = 1 + np.argmin(S[1:,0] / np.maximum(pivotCol[1:], 0))
+  basis[pivotRowIdx-1] = pivotColIdx-1
   S[pivotRowIdx] = S[pivotRowIdx] / pivotCol[pivotRowIdx]
   for i in range(S.shape[0]):
     if i == pivotRowIdx:
       continue
     S[i] -= pivotCol[i]*S[pivotRowIdx]
   print(S)
+  print(basis)
 print(status)
